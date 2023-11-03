6e1a3de41c2d5174cfd76bf69cdfdc84c86e2692
fe4960 <wjeeuc1@gmail.com>
2023-11-03 12:01:25 -0500

    Delete analysis/Figure4/MPRA directory
    This directory was moved to utility.

--- analysis/Figure4/MPRA/0file_list.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-date=$1
-ls /storage/chenlab/Users/junwang/enhancer_validation/data/${date}/*R1* | awk -F "." '{print $1}' | awk -F "/" '{print $NF}' > /storage/chenlab/Users/junwang/enhancer_validation/data/${date}/file_list
-
-
--- analysis/Figure4/MPRA/6summarize_enhancer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#ll /storage/chenlab/Users/junwang/human_meta/data/proj2/h5ad/ | grep h5ad | awk '{print $NF}' | sed -e "s/_flt.h5ad//g" | sort -u > /storage/chenlab/Users/junwang/human_meta/data/proj2/snATAC_list
-export PYTHONPATH=""
-source /storage/chen/home/jw29/software/anaconda3/bin/activate  scvi-env
-bc=$1
-gRNA=$2
-spe=$3
-date=$4
-python enhancer_validation/scripts/6summarize_enhancer.py $bc $gRNA $spe $date
